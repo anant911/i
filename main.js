7088737530478f3af85a52d86bfab701276c288f
anant911 <77596400+anant911@users.noreply.github.com>
2022-05-16 21:55:37 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+canvas=createCanvas(700,600);
+canvas.parent("canvas");
+video=createCapture(VIDEO);
+video.size(700,600);
+video.hide();
+
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose', gotPoses)
+}
+
+function draw()
+{
+image(video, 0,0,700,600);
+
+}
+
+function modelLoaded()
+{
+    console.log('model is loaded');
+}
+
